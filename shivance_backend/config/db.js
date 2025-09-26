2cc89e785732ab2125372318a74e35428bdd9077
AVI <theravikumar15@gmail.com>
2025-09-26 21:50:19 +0530

    Delete shivance_backend directory

--- shivance_backend/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = async (uri) => {
-  await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-  console.log('MongoDB connected');
-};
-
-module.exports = connectDB;
